0cd745ef4d9a4b6746eed7d045b67e376e86d5f7
Felip322 <120796936+Felip322@users.noreply.github.com>
2025-08-27 15:19:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ from itsdangerous import URLSafeTimedSerializer
 
 # Config e-mail (exemplo com Gmail, mas pode usar SendGrid, Mailtrap etc.)
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
-app.config["MAIL_PORT"] = 587
+app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USER")   # seu e-mail
 app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASS")   # senha/app password
