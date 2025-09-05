4cc1a8bf20562ff5a6513df1e627a3458d921bbe
Felip322 <120796936+Felip322@users.noreply.github.com>
2025-09-05 19:54:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -509,13 +509,13 @@ def weekly_event_start():
         return redirect(url_for("weekly_event"))
 
     # Cria jogo do evento semanal com 10 perguntas
-    g = Game(
-        user_id=user.id,
-        rounds_count=10,
-        themes_json=json.dumps([key for key, _ in THEMES]),
-        mode="weekly",
-        event_id=event.id  # <-- salvar o evento correto no Game
-    )
+   g = Game(
+    user_id=user.id,
+    rounds_count=10,
+    themes_json=json.dumps([key for key, _ in THEMES]),
+    mode="weekly"
+)
+
     db.session.add(g)
     db.session.commit()
 
